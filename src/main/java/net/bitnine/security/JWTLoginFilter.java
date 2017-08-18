2e3fc7b201082deef3f8d2cfa2a8078f0d51d3d4
lionkim <cppcode@naver.com>
2017-08-18 17:26:54 +0900

    copying

---
+++ src/main/java/net/bitnine/security/JWTLoginFilter.java
@@ -0,0 +1,33 @@
+package net.bitnine.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+    
+    public JWTLoginFilter (String url, AuthenticationManager authManager) {
+        super (new AntPathRequestMatcher(url));
+        setAuthenticationManager (authManager);
+    }
+    
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        
+        AccountCredentials creds = new ObjectMapper().readValue(request.getInputStream(),
+                AccountCredentials.
+    }
+}
